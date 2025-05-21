50195cda9a286b43b9db9daaf945e4860bc6bce7
shanktt <ashank@penrose.com>
2025-05-21 11:10:23 -0700

    Initial Commit

---
+++ RecorderWithFilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecorderWithFilter
+//
+//  Created by Ashank Tomar on 5/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
